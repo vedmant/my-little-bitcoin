88a6a0085b73e65517d9258419ca2ebb9075672b
Vedmant <vedmant@gmail.com>
2019-06-28 23:43:25 +0300

    Added Sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
+const Sentry = require('@sentry/node')
 global.debug = require('debug')('app:global')
 const config = require('./config')
 const bus = require('./bus')
 const store = require('./store')(config, bus)
 const miner = require('./miner')(config, bus, store)
 require('./server')(config, bus, store, miner).start()
+
+if (process.env.APP_ENV === 'production') {
+  Sentry.init({dsn: process.env.SENTRY_DSN})
+}
 
 if (config.demoMode) {
   miner.mine(store.wallets[0])
